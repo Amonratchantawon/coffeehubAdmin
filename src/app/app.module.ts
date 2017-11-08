371baa15b716e0ca80731571c2fbc3b5f1c85b0f
amonratCha <amonrat.cha@northbkk.ac.th>
2017-11-08 10:50:42 +0700

    login

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,13 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { ShopComponent } from './shop/shop.component';
+import { LoginComponent } from './login/login.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ShopComponent
+    ShopComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule
